bec01074289f59ca044a0327ca902d05b6f3dac2
MaouNour <nourg0870@gmail.com>
2025-12-08 09:47:01 +0300

    nevermind you just need to re type the state['blab']['blab] because it is hardcoded rn

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'screens/sign_up_screen.dart';
 import 'screens/success_screen.dart';
 import 'screens/home_page_screen.dart';
 
+//blab
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
